b5cbe85c8db0827312af4575c45c0f43743874be
farha <farhajw@gmail.com>
2021-04-24 15:30:28 -0700

    string to int

---
+++ MyAtoi.java
@@ -0,0 +1,55 @@
+
+public class MyAtoi {
+	
+	 public int myAtoi(String s) {
+		 
+		 boolean isPositive = true;
+		 boolean isNotDigit = true;
+		 char[] chars = s.toCharArray();
+		 String output = "";
+		 int num = 0;
+		 
+		 for(char c: chars) {
+			 if(Character.isDigit(c)) {
+				 isNotDigit = false;
+				 output+=c;
+			 }
+			 else if(c == '-' && isNotDigit) {
+				 isPositive = false;
+				 isNotDigit = false;
+			 }
+			 else if(c == '+' && isNotDigit) {
+				 isPositive = true;
+				 isNotDigit = false;
+			 }
+			 else if(c == ' ' && isNotDigit) {
+				 continue;
+			 }
+			 else {
+				 break;
+			 }
+		 }
+		 try {
+			 num = output.length() > 0? Integer.parseInt(output) : num;
+		 }
+		 catch (NumberFormatException e) {
+			 if(isPositive) {
+				 num = Integer.MAX_VALUE;
+			 }
+			 else {
+				 num = Integer.MIN_VALUE;
+			 }
+		 }   
+		 
+		 if(!isPositive) {
+			 num = -num;
+		 }
+		 return num;
+	 }
+	 
+	 public static void main(String[] args) {
+		 MyAtoi ma = new MyAtoi();
+		 System.out.println(ma.myAtoi("+-12"));
+	 }
+
+}
